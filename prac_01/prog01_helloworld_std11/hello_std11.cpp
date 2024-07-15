4cde222151f3607815bb98ae098788c55c133b30
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-07-15 16:27:47 +1000

    Studying how the C++ code works in C++ 17

--- prac_01/prog01_helloworld_std11/hello_std11.cpp
+++ prac_01/prog01_helloworld_std11/hello_std11.cpp
@@ -1,15 +1,25 @@
+// This is a hello world program
+// C++ 17
+
 #include <iostream>
 #include <vector>
 #include <string>
+// import library first
 
 using namespace std;
 
+// int main() is where the whole program begins
 int main()
 {
+    // For making a var
+    // give a var type followed by var name
+    // datatype varname {"Hello World"}
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
+    // data is constant
     for (const string& word : msg)
     {
+        // cout = python's print()
         cout << word << " ";
     }
     cout << endl;
