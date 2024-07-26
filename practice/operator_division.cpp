ddd23059db7fbe8246bbb5ef1b4cdb412111253d
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-07-26 10:32:55 +1000

    operator division: divide with int and mixed division

---
+++ practice/operator_division.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Integer division: 9/5 = " << 9/5 << endl;
+    cout << "Mixed division: 9.0/5 = " << 9.0/5 << endl;
+
+    return 0;
+}
