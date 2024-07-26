0e402d9ed62cad5d05c0fe0c9facde0b6486bf1e
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-07-26 10:44:20 +1000

    function test

--- practice/operator_division.cpp
+++ practice/operator_division.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int print_divide()
+{
+    cout << "Test" << 10 / 5 << endl; 
+}
+
 int main()
 {
     cout << "Integer division: 9/5 = " << 9/5 << endl;
     cout << "Mixed division: 9.0/5 = " << 9.0/5 << endl;
     cout << "Floating divison: 9.0/5.0 = " << 9.0 / 5.0 << endl;
-
+    cout << print_divide << endl;
     return 0;
 }
