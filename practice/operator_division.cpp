18d9482c02a0ee8f3738b82e415c844173c2cce4
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-07-26 10:37:10 +1000

    Divide float

--- practice/operator_division.cpp
+++ practice/operator_division.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout << "Integer division: 9/5 = " << 9/5 << endl;
     cout << "Mixed division: 9.0/5 = " << 9.0/5 << endl;
+    cout << "Floating divison: 9.0/5.0 = " << 9.0 / 5.0 << endl;
 
     return 0;
 }
