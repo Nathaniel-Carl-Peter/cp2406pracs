b24ed071adf90d679cdc30090791058392d74dec
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-08-06 16:18:03 +1000

    practiceBasics_01

---
+++ practice/arithmatic operand/practiceExcercisebasics.cpp
@@ -0,0 +1,22 @@
+/*
+Perform basic math operators
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int add = 9 + 10;
+    int subtraction = 4-2;
+    int float_div = 9.0 / 3.0 ;
+    cout << "Integer division: 9/5 = " << 9/5 << endl;
+    cout << "Mixed division: 9.0/5 = " << 9.0/5 << endl;
+    cout << "Floating divison: 9.0/5.0 = " << 9.0 / 5.0 << endl;
+    
+    cout << add << endl;
+    cout << subtraction << endl;
+    cout << float_div << endl;
+
+    // cout << print_divide() << endl;
+}
---
+++ practice/helloworld/practiceExcercisebasics.cpp
@@ -0,0 +1,21 @@
+/*
+Create a hello world by:
+    -Assigning a var
+    -using cout
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    string value = "Hello World C++";
+    cout << value << endl;
+    // vector<string> snake = {"SNAKE", "EATER", "!", "from", "VS Code", "and the C++ extension!"};
+    // for (const string& word : snake)
+    // {
+    //     // cout = python's print()
+    //     cout << snake << endl; 
+    // }
+    return 0;
+}
